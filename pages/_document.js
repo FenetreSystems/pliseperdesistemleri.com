9cdbf0b7fb828d9fd00a9abe4e88375ce310cbfb
Plise Perde Dev <dev@pliseperdesistemleri.com>
2026-05-04 18:15:00 +0300

    Feat: Self-host Inter font locally - Remove Google Fonts dependency

--- pages/_document.js
+++ pages/_document.js
@@ -7,8 +7,6 @@ export default function Document() {
         <meta charSet="utf-8" />
         <meta name="theme-color" content="#83bd81" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link rel="dns-prefetch" href="https://www.google-analytics.com" />
         <link rel="dns-prefetch" href="https://www.googletagmanager.com" />
       </Head>
